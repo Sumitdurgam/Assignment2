47b10938ec321f6eacad95ccfeceb09df905a9a4
Sumitdurgam <sumitdurgam12@gmail.com>
2025-05-17 17:27:59 +0530

    assignment2.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
 function App () {
   const [data, setData] = useState({employees: [], products:[]});
 
   useEffect(() => {
-    axios.get('https://dummyjson.com/products')
+    axios.get("http://localhost:3001/combined-data")
     .then(response => setData(response.data))
     .catch(error => 
       console.error("Error fetching data", error));
